73d688c40e266a52cbf26c1b6e07dab73bf645d7
fivaayu <ezayayang@gmail.com>
2020-02-17 08:54:50 +0700

    membuat Market Place

--- app/src/main/java/id/putraprima/marketplacelayout/profil.java
+++ app/src/main/java/id/putraprima/marketplacelayout/profil.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class profil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profil);
     }
 
     public void postLogin(View view) {
-        Intent intent = new Intent(this, login.class);
+        Intent intent = new Intent(this, register.class);
         startActivity(intent);
     }
 }
